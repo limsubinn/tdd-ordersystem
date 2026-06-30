4d898d1f7f77faae08910c8769508539a3ae8e62
limsubinn <mymyx02@naver.com>
2026-06-30 21:17:29 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/example/tddordersystem/TddOrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddOrdersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
